c22fa25f8e25e7e224f5ca0ee0c87b21bcbb8a9a
Vahan <vahan.arakelyan@betconstruct.com>
2017-11-30 19:19:38 +0400

    ADD Loading page for React;some new animations

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,49 +1,59 @@
 const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
 
 const path = require('path');
 const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow, loadingWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 800,
-        minWidth: 1000,
-        minHeight: 600,
+    loadingWindow = new BrowserWindow({
+        width: 640,
+        height: 400,
         backgroundColor: '#f1f1eb',
-        icon: path.join(__dirname, '../public/electron-app/main-icon.png'),
+        show: false,
+        frame: false
     });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
-
-    mainWindow.webContents.on("did-fail-load", () => setTimeout(() => mainWindow.loadURL('http://localhost:3000'), 1000));
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-        app.quit();
+    // Create the browser window.
+    loadingWindow.once('show', () => {
+        mainWindow = new BrowserWindow({
+            width: 1280,
+            height: 800,
+            minWidth: 1000,
+            minHeight: 600,
+            backgroundColor: '#f1f1eb',
+            icon: path.join(__dirname, '../public/electron-app/main-icon.png'),
+            show: false
+        });
+    
+        mainWindow.webContents.once('dom-ready', () => {
+            mainWindow.show();
+            loadingWindow.hide();
+            loadingWindow.close();
+        });
+        
+        setTimeout(() => mainWindow.loadURL('http://localhost:3000'), 1000);
+        
+        mainWindow.on('closed', function () {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+            app.quit();
+        });
     });
+
+    loadingWindow.loadURL(`file://${__dirname}/loading.html`);
+    loadingWindow.show();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-/*app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-        app.quit();
-});*/
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
